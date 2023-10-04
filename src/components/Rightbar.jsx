cf35dae404fd83cba888efb5b50ec9cf3160ac2b
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-04 22:55:51 +0530

    added

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,12 +1,19 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import React from "react";
 
 export default function Rightbar() {
   return (
-    <Box bgcolor={"pink"} flex={2} p={2} sx={{display:{xs:'none',sm:'block'}}}>
-     <Box position="fixed">
-     <Typography variant='h6' fontWeight='200'>Online Friends</Typography>
-     </Box>
+    <Box
+      bgcolor={"pink"}
+      flex={2}
+      p={2}
+      sx={{ display: { xs: "none", sm: "block" } }}
+    >
+      <Box position="fixed">
+        <Typography variant="h6" fontWeight="200">
+          Online Friends
+        </Typography>
+      </Box>
     </Box>
-  )
+  );
 }
