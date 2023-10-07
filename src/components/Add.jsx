8b9b0bad83bf6834a1e3836745c758ef4cc244e9
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-07 23:44:26 +0530

    working on add component

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import { Fab, Tooltip } from "@mui/material";
+import { Add as AddIcon } from "@mui/icons-material";
+import React from "react";
 
 export default function Add() {
   return (
     <div>
-      add
+      <Tooltip title="Add">
+        <Fab sx={{ color: "blue" }} aria-label="add">
+          <AddIcon sx={{ color: "blue" }} />
+        </Fab>
+      </Tooltip>
     </div>
-  )
+  );
 }
-
