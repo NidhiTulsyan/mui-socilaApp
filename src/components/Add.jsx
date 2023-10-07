803a3dbfa5d4444a80189ffc42b5f3bcdd7aa887
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-07 23:50:09 +0530

    working on add component

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -4,12 +4,12 @@ import React from "react";
 
 export default function Add() {
   return (
-    <div>
-      <Tooltip title="Add">
-        <Fab sx={{ color: "blue" }} aria-label="add">
-          <AddIcon sx={{ color: "blue" }} />
+    <>
+      <Tooltip title="Add" sx={{position:'fixed',bottom:20,left:30}}>
+        <Fab color="primary" aria-label="add">
+          <AddIcon  />
         </Fab>
       </Tooltip>
-    </div>
+    </>
   );
 }
