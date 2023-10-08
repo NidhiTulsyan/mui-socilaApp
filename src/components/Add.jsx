55551a6c37a9fb7c657bd364537cca065a91d5c5
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-08 23:24:32 +0530

    working on add plus button of  modal

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,4 +1,4 @@
-import { Fab, Tooltip } from "@mui/material";
+import { Box, Fab, Modal, Tooltip, Typography } from "@mui/material";
 import { Add as AddIcon } from "@mui/icons-material";
 import React from "react";
 
@@ -17,6 +17,21 @@ export default function Add() {
           <AddIcon />
         </Fab>
       </Tooltip>
+      <Modal
+  open={open}
+  onClose={handleClose}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box sx={style}>
+    <Typography id="modal-modal-title" variant="h6" component="h2">
+      Text in a modal
+    </Typography>
+    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+    </Typography>
+  </Box>
+</Modal>
     </>
   );
 }
