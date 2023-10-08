af08a828d3fcdce71816fff408128ac3c2f99445
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-09 00:17:00 +0530

    working on theme of app dark/light

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { Box, Stack } from "@mui/material";
-import React from "react";
+import { Box, Stack, ThemeProvider, createTheme } from "@mui/material";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
 import Rightbar from "./components/Rightbar";
 import Add from "./components/Add";
 
+
 export default function App() {
+  const [mode,setmode] = useState("light")
+const DarkTheme = createTheme({
+  palette:{
+    mode:mode
+  }
+})
   return (
+    <ThemeProvider theme={DarkTheme}>
     <Box>
       <Navbar />
       <Stack direction="row" justifyContent="space-between" spacing={2}>
@@ -17,5 +25,6 @@ export default function App() {
       </Stack>
       <Add />
     </Box>
+    </ThemeProvider>
   );
 }
